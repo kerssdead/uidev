14945ca35c66f4e227fc4a3e8c7bdf0ab80fba8c
Semen <bethesda.stillxxxx@gmial.com>
2022-10-14 13:32:11 +0300

    1410200-01: toggle switch

--- scripts.js
+++ scripts.js
@@ -1,41 +1,38 @@
-$(document).ready(function () {
-    if (parseInt($('body')[0].scrollHeight.slice(0, -2)) > parseInt($('body')[0].clientHeight.slice(0, -2))) {
-        $('#footer').css('position', 'absolute');
-    }
+// $(document).ready(function () {
+//     if (parseInt($('body')[0].scrollHeight.slice(0, -2)) > parseInt($('body')[0].clientHeight.slice(0, -2))) {
+//         $('#footer').css('position', 'absolute');
+//     }
+// });
 
-    let buttonToTop = docuemnt.getElementById('button-to-top');
-    buttonToTop.offsetWidth = window.internalWidth - 32;
-    buttonToTop.offsetHeight = window.internalHeight - 32;
-
-    buttonToTop.addEventListener('scroll', ev => {
-        if (document.scrollHeight > '250px') {
-            buttonToTop.classList.remove('hidden');
-        } else {
-            buttonToTop.classList.add('hidden');
-        }
-    })
-});
+// document.onreadystatechange = ev => {
+//     if (document.readyState == 'complete') {
+//         if (document.body.scrollHeight > document.body.clientHeight) {
+//             document.getElementById('footer').style.position = 'absolute'
+//         }
+//     }
+// }
 
 function openFind() {
-    $('#navigation-buttons-0').removeClass('find-button-hide');
-    $('#navigation-buttons-0').removeClass('find-button-show');
-    $('#find-row').removeClass('find-button-show');
-    $('#find-row').removeClass('find-button-hide');
+    let nav = document.getElementById('navigation-buttons-0')
+    let row = document.getElementById('find-row')
 
-    if (!$('#navigation-buttons-0').hasClass('hidden')) {
-        $('#navigation-buttons-0').addClass('find-button-hide');
+    nav.classList.remove('find-button-hide', 'find-button-show')
+    row.classList.remove('find-button-show', 'find-button-hide')
+
+    if (!nav.classList.contains('hidden')) {
+        nav.classList.add('find-button-hide')
         setTimeout(() => {
-            $('#navigation-buttons-0').toggleClass('hidden');
-            $('#find-row').toggleClass('hidden');
-            $('#find-row').addClass('find-button-show');
-        }, 799);        
-    } else {
-        $('#find-row').addClass('find-button-hide');
-        console.log('hide');
+            nav.classList.toggle('hidden')
+            row.classList.toggle('hidden')
+            row.classList.add('find-button-show')
+        }, 799)
+    }
+    else {
+        row.classList.add('find-button-hide')
         setTimeout(() => {
-            $('#find-row').toggleClass('hidden');
-            $('#navigation-buttons-0').toggleClass('hidden');
-            $('#navigation-buttons-0').addClass('find-button-show');
-        }, 799);
+            row.classList.toggle('hidden')
+            nav.classList.toggle('hidden')
+            nav.classList.add('find-button-show')
+        }, 799)
     }
 }
