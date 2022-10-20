290e8589b6dd7c7dca468b4d54497ba977c183e7
Semen <bethesda.stillxxxx@gmial.com>
2022-10-20 13:06:56 +0300

    201022-04: improved modal window :arrow_up::arrow_up:/play crickets

--- scripts.js
+++ scripts.js
@@ -95,11 +95,37 @@ function slider() {
 
 
 function toggleModal() {
-    document.querySelector('.modal-container').classList.toggle('hidden')
+    let c = document.querySelector('.modal-container')
+    c.classList.toggle('hidden')
+    if (!c.classList.contains('hidden')) {
+        document.querySelectorAll('i.star-active').forEach(v => {
+            v.classList.remove('star-active')
+            v.classList.add('star')
+        })
+    }
 }
 
 function scrollToTop() {
     window.scrollTo({
         top: 0, behavior: 'smooth'
+    })
+}
+
+function modal() {
+    let list = document.querySelectorAll('i.star')
+    list.forEach((v, i) => {
+        v.addEventListener('click', ev => {
+            for (let j = 0; j < list.length; j++) {
+                const child = list[j]
+                if (j <= i) {
+                    child.classList.remove('star')
+                    child.classList.add('star-active')
+                }
+                else {
+                    child.classList.remove('star-active')
+                    child.classList.add('star')
+                }
+            }
+        })
     })
 }
